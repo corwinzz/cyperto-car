e050ff5a7790ae1191935b3af3d48d1f10f05fab
zjlmmm-github <zjlmmm2@126.com>
2021-12-09 23:59:07 +0800

    禁止zoom

--- src/components/Three3D/ThreeCardBloom.vue
+++ src/components/Three3D/ThreeCardBloom.vue
@@ -174,6 +174,7 @@ export default Vue.extend({
             mDom.appendChild(t.renderer.domElement)
             t.addBloomPass()
             t.controls = new OrbitControls(t.camera, t.renderer.domElement)
+            t.controls.enableZoom = false
             t.controls.dampingFactor = 0.25
             t.controls.minDistance = 0.001
             t.controls.maxDistance = 10000
